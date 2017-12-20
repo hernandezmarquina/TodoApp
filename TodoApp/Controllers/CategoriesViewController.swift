f035fc790bf236a1b47f300a5e49bcde0a5d767d
Jonathan Hernandez <jonathan@Jonathans-MacBook-Air.local>
2017-12-20 15:26:11 -0700

    save items using coreData

--- TodoApp/Controllers/CategoriesViewController.swift
+++ TodoApp/Controllers/CategoriesViewController.swift
@@ -7,45 +7,53 @@
 //
 
 import UIKit
+import CoreData
 
 class CategoriesViewController: UITableViewController {
     
-    var itemArray = [Item]()
+    var managedObjectContext : NSManagedObjectContext?
+    var fetchedResultsController : NSFetchedResultsController<Item>!
     
     let dataFilePath = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first?.appendingPathComponent("Items.plist")
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        // Local data File path
+        //print(FileManager.default.urls(for: .documentDirectory, in: .userDomainMask))
+        
+        managedObjectContext = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
+        
         loadItems()
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
         let cell  = tableView.dequeueReusableCell(withIdentifier: "toDoItemCell", for: indexPath)
-        let item = itemArray[indexPath.row]
+        let item = fetchedResultsController.object(at: indexPath)
         
         cell.accessoryType  = item.done ? .checkmark : .none
-        cell.textLabel?.text = itemArray[indexPath.row].title
+        cell.textLabel?.text = item.title
         
         return cell
     }
     
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return itemArray.count
+        return fetchedResultsController.sections?[section].numberOfObjects ?? 0
     }
     
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         
         let tableCell = tableView.cellForRow(at: indexPath)
-        let item = itemArray[indexPath.row]
+        let item = fetchedResultsController.object(at: indexPath)
         
         item.done = !item.done
         tableCell?.accessoryType  = item.done ? .checkmark : .none
         
         tableView.deselectRow(at: indexPath, animated: true)
         
-        saveItems()
+        updateItem()
         
     }
     
@@ -63,17 +71,10 @@ class CategoriesViewController: UITableViewController {
         
         let action = UIAlertAction(title: "Add Item", style: .default) {(action) in
             
-            let newitem = Item()
-            
-            newitem.title = textField.text ?? "New Item"
-            
-            self.itemArray.append(newitem)
-            
-            self.saveItems()
+            self.saveItem(title: textField.text ?? "New Item")
         }
         
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel) {(action) in
-            
             
         }
         
@@ -84,36 +85,50 @@ class CategoriesViewController: UITableViewController {
         
     }
     
-    func saveItems(){
-        let encoder = PropertyListEncoder()
+    func saveItem(title: String){
+       
+        let item = Item(context: managedObjectContext!)
+        item.title = title
+        item.done = false
         
         do {
-            
-            let data = try encoder.encode(itemArray)
-            try data.write(to: dataFilePath!)
-            
+          try managedObjectContext?.save()
         } catch {
             print("Error encoding item array, \(error)")
         }
         
-        tableView.reloadData()
+        loadItems()
+    }
+    
+    func updateItem(){
+        
+        do {
+            try managedObjectContext?.save()
+        } catch {
+            print("Error encoding item array, \(error)")
+        }
+        
+        loadItems()
     }
     
     func loadItems(){
         
-        if let data = try? Data(contentsOf: dataFilePath!) {
-            
-            let decoder = PropertyListDecoder()
-            
+        
+        let fetchRequest : NSFetchRequest<Item> = Item.fetchRequest()
+        fetchRequest.sortDescriptors = [NSSortDescriptor (key: "title", ascending: true)]
+        
+        fetchedResultsController = NSFetchedResultsController(fetchRequest: fetchRequest,
+                                                              managedObjectContext: managedObjectContext!, sectionNameKeyPath: nil, cacheName: nil)
+        
             do {
                 
-            itemArray = try decoder.decode([Item].self, from: data)
+                try fetchedResultsController.performFetch()
                 
             } catch {
-                
                 print("Error decoding item array \(error)")
             }
-        }
+        
+        tableView.reloadData()
         
     }
     
